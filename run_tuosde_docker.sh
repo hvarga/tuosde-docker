3b2d878aae7228c39071800047d5896ac9bb92d0
Hrvoje Varga <hrvoje.varga@gmail.com>
2020-05-18 19:25:45 +0200

    add support for a permanent storage
    Signed-off-by: Hrvoje Varga <hrvoje.varga@gmail.com>

--- run_tuosde_docker.sh
+++ run_tuosde_docker.sh
@@ -9,6 +9,21 @@
 # - Change your working directory to a root of your project and run this script
 #   by typing run_tuosde_docker.sh from your terminal.
 
+# Check arguments.
+if [ "$#" -gt 1 ]; then
+	echo "Illegal number of arguments."
+	echo "Script expects one or no arguments."
+	echo "If an argument is given, it is used as a path to a storage."
+	exit 1
+fi
+
+# Set storage path.
+STORAGE_PATH=$HOME/tuosde-docker
+if [ "$#" -eq 1 ]; then
+	STORAGE_PATH="$1"
+fi
+mkdir "$STORAGE_PATH" 2> /dev/null
+
 docker run --privileged --network host -it --rm \
 	-e USER_ID=$(id -u) \
 	-e GROUP_ID=$(id -g) \
@@ -21,4 +36,5 @@ docker run --privileged --network host -it --rm \
 	-v /tmp/.X11-unix:/tmp/.X11-unix \
 	-v "$HOME"/.Xauthority:/var/run/xauthority \
 	-v "$HOME"/.gitconfig:/etc/gitconfig \
+	-v "$STORAGE_PATH":/opt/storage \
 	hvarga/tuosde-docker
